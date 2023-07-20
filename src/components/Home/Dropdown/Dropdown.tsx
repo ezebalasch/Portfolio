904a3e7e475ea629db84b027c051125737cc705c
Ezequiel Balasch <ezebalasch@gmail.com>
2023-07-20 19:11:40 -0300

    Profile Card and Experience Dropdown

---
+++ src/components/Home/Dropdown/Dropdown.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import "./Dropdown.css";
+import { FaAngleDown, FaAngleUp, FaLink } from "react-icons/fa6";
+
+const workExperience = [
+  {
+    title: "Programming Teacher",
+    company: "Queen Mary College",
+    date: "02/2023 – present",
+    link: "",
+    description:
+      "Carry out monitoring and continuous evaluation of the teaching and learning process. Prepare and update teaching materials. Participate in training and training activities.",
+  },
+  {
+    title: "Web Developer Freelance",
+    company: "Buena Seguridad",
+    date: "12/2022 – 01/2023",
+    link: "https://www.buenaseguridad.com.ar/",
+    description:
+      "Development of an application using Javascript and React. Design and construction of an attractive and functional user interface. Adaptable to mobile devices.",
+  },
+  {
+    title: "Programming Teacher",
+    company: "Newton School",
+    date: "05/2022 – 02/2023",
+    link: "",
+    description:
+      "Plan and design the study program of the subject. Prepare and teach theoretical and practical classes. Conduct tutorials and assist students in their doubts and queries.",
+  },
+];
+
+const DropdownItem = ({ work }) => {
+  const [opened, setOpened] = useState(false);
+  const handleOnClick = () => {
+    setOpened(!opened);
+  };
+  return (
+    <>
+      <button onClick={handleOnClick} className="dropdown-experience">
+        {work.title} {opened ? <FaAngleUp /> : <FaAngleDown />}
+      </button>
+      <div className={`dropdown-content ${opened ? "opened" : ""}`}>
+        <div className="dropdown-opened-card">
+          <div className="dropdown-opened-card-company">
+            {work.company}
+            {work.link !== "" ? (
+              <span>
+                <a className="linksite" href={work.link}>
+                  <FaLink />{" "}
+                </a>
+              </span>
+            ) : (
+              ""
+            )}
+
+            <div className="dropdown-opened-card-description">
+              {work.description}
+            </div>
+          </div>
+          <div className="dropdown-opened-card-date">{work.date}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Dropdown = () => {
+  return (
+    <div className="about-experience-content">
+      {workExperience.map((work, index) => (
+        <DropdownItem key={index} work={work} />
+      ))}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,59 +1,49 @@
 import "./Home.css";
 import { Fade } from "react-awesome-reveal";
 import perfil from "./../../assets/perfil.png";
+import Dropdown from "./Dropdown/Dropdown";
 
 const Home = () => {
   return (
     <>
       <div className="header-container">
-        <Fade direction="down" cascade>
+        <Fade direction="down" cascade triggerOnce={true} reset={true}>
           <div className="header">Hi! I'm Ezequiel Balasch</div>
           <div className="header-subtitle">Full Stack Web Developer</div>
         </Fade>
       </div>
       <div className="profile-container">
-        <Fade direction="left" cascade>
-          <div className="profile-card">
-            <div className="profile-card-description">
-              s sint aliquid odit quo beatae illo iure repellat, voluptate at id
-              culpa. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Ipsa facilis vel nam cumque, similique reiciendis accusamus quidem
-              fugit omnis ea molestias soluta delectus totam at natus? Vitae
-              quod laboriosam illo! Lorem ipsum dolor sit, amet consectetur
-              adipisicing elit. Vel fugit facere fuga quidem quaerat ipsam
-              blanditiis
-            </div>
-            <div className="profile-card-img">
-              <img
-                className="image-profile"
-                src={perfil}
-                alt="Ezequiel Balasch"
-              />
+        <Fade direction="up" duration={800} triggerOnce={true} reset={true}>
+          <div className="profile-card-container">
+            <div className="profile-card">
+              <div className="profile-card-img">
+                <img
+                  className="image-profile"
+                  src={perfil}
+                  alt="Ezequiel Balasch"
+                />
+              </div>
+              <div className="profile-card-description">
+                I live in Mendoza, Argentina.I am an enthusiastic Full Stack
+                Programmer with experience in Java, JavaScript, React, Thymeleaf
+                and Spring Boot. I also have a solid track record as a
+                programming teacher at the primary and secondary levels, which
+                has allowed me to develop exceptional communication and teamwork
+                skills. My passion for programming motivates me to continue
+                growing and learning in this industry. I have an advanced level
+                of English, which allows me to interact effectively in
+                international environments.
+              </div>
             </div>
           </div>
         </Fade>
-        {/* <Fade direction="right" cascade>
-          <div className="profile-card">
-            <div className="profile-card-description">
-              s sint aliquid odit quo beatae illo iure repellat, voluptate at id
-              culpa. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Ipsa facilis vel nam cumque, similique reiciendis accusamus quidem
-              fugit omnis ea molestias soluta delectus totam at natus? Vitae
-              quod laboriosam illo! Lorem ipsum dolor sit, amet consectetur
-              adipisicing elit. Vel fugit facere fuga quidem quaerat ipsam
-              blanditiis
-            </div>
-            <div className="profile-card-img">
-              <img
-                className="image-profile"
-                src={perfil}
-                alt="Ezequiel Balasch"
-              />
-            </div>
-          </div>
-        </Fade> */}
       </div>
-      <div className="about-container">row</div>
+      <div className="about-container">
+        <div className="about-card">
+          <div className="about-header">Experience</div>
+          <Dropdown />
+        </div>
+      </div>
       <div className="projects-container">row</div>
 
       <div className="home">
